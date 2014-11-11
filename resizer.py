61b8ad9e98e90199d81be182dbc4d47b7ebea2fd
Arindam Roychowdhury <arindam@bigbasket.com>
2014-11-11 18:03:35 +0530

    get_images_listed tested..working fine

--- resizer.py
+++ resizer.py
@@ -2,17 +2,22 @@ import PIL
 from PIL import Image
 import os
 
-def getfiles(loc):
+def get_images_list(loc):
     """ Get only image files """
     all_files_list = os.listdir(loc)
-    accepted_formats_tuple = (bmp, jpg, jpeg)
+    accepted_formats_tuple = ('bmp', 'jpg', 'jpeg', 'png')
     final_files_list = []
-    for f in all
+    if all_files_list:
+        for f in all_files_list:
+            if f.split('.')[-1] in accepted_formats_tuple:
+                final_files_list.append(f)
+    return final_files_list
+    
     
 
-def resize(loc): # loc is folder location
-    files = getfiles(loc)
-    img = Image.open(loc)
+def resize_image(f, basewidth):  # f is filename with complete path
+    img = Image.open(f)
+    wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-    img.save(new_name)
+    img.save(img)
